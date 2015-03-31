efb13a0d4ef6ba671c65de1833910a65985cd410
Dennis Lu <dennislu@fashionguide.com.tw>
2015-03-31 19:04:59 +0800

    v1.rb

--- lib/api/v1.rb
+++ lib/api/v1.rb
@@ -1,10 +1,21 @@
 
 module Api
   class V1 < Sinatra::Application
+    get '/:channel/:token' do
+    	begin
+    		
+    		task = Task.includes(:channel).where(token: params[:token], channels: { name: params[:channel] })
+    		if !task.exists?
+    			raise 'data error'
+    		end
 
-    get '/' do
-      'API'
+    		task = task.take
+    		task.count++
+    		task.save
+
+    	rescue
+    		puts $!
+    	end
     end
-
   end
 end
