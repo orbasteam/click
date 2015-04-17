3d02b9420b54dccd2fc1b35260d603c877d6bd90
Dennis Lu <dennislu@fashionguide.com.tw>
2015-04-17 18:31:12 +0800

    redis 2

--- lib/api/v1.rb
+++ lib/api/v1.rb
@@ -9,10 +9,13 @@ module Api
     			raise 'data error'
     		end
 
-            pk = $redis.incr 'logs'
-            $redis.hset 'logs', params[:token];
-            puts 'done'
-            # redirect task.take.target_url
+          #(1..50).to_a.each do |num|
+          #  $redis.del "logs:#{num}"
+          #end
+
+          id = $redis.incr 'logs'
+          $redis.mapped_hmset "logs:#{id}", { id: id, ip: request.env['REMOTE_ADDR'], token: params[:token], click_time: Time.now.strftime("%Y/%m/%d %H:%M:%S") };
+          # redirect task.take.target_url
 
     	rescue
     		p $!
